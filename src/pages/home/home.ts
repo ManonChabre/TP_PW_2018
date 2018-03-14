36165d07977b3d8fb474f535215237f3096e90b6
chabrema <manon.chabre@gmail.com>
2018-03-14 16:55:06 +0100

    Barre de recherche ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+export interface Results{
+  title : string;
+  author : string;
+  date : string;
+  image : string;
+}
 
+const result : Results[]= [
+  {title : 'Coco', author: 'Moi', date : 'Aujourdhui ', image : 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/8AECsRLEDLoNvZ8JqrTFm0zBPqH.jpg'},
+  {title : 'Le labyrinthe', author: 'Moi ', date : 'Aujourdhui ', image : 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/7h9LUexxkTSX9YWSQ88b6PE1JdL.jpg'},
+  {title : 'Aléatoire', author: 'Moi ', date : 'Aujourdhui ', image : 'https://picsum.photos/200/300/?random'}
+];
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    results : Results[];
 
+  constructor(public navCtrl: NavController) {
+      this.results= [];
+  }
+
+  onInput(event:any):void{
+   const query : string = event.target.value;
+   this.results=query?result:[];
   }
 
 }
