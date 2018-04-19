eb5a58a853007d2c8f73ee6d4273b80c4579150f
chabrema <manon.chabre@gmail.com>
2018-04-19 21:14:40 +0200

    Fin projet

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,5 @@
+import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/operator/switchMap';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/pluck';
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { DetailsPage } from '../details/details';
 import {Observable} from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
+import { AlertController, Platform } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
 import { key } from '../../app/tmdb';
+import { Subscription } from 'rxjs/Subscription';
+import { Shake } from '@ionic-native/shake';
 
 export interface Result{
   overview : string;
   title : string;
-  popularity : number;
+  vote_average : number;
   release_date : string;
   poster_path : string;
 }
@@ -17,17 +21,18 @@ export interface Result{
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+    shakeSubscription : Subscription;
     results : Observable<Result[]>;
     pushPage : typeof DetailsPage= DetailsPage;
-
-  constructor(public http: HttpClient) {
-      this.results= Observable.of([]);
+   
+  constructor(public http: HttpClient, public alert : AlertController,public navCtrl: NavController, public platform : Platform, public shake : Shake ) {
+      this.results = Observable.of([]);
   }
 
   onInput(event:any):void{
    const query : string = event.target.value;
-   this.results=query? this.fetchResults(query) : Observable.of([]);
+  // this.results=query? this.fetchResults(query) : Observable.of([]);
+   this.results=query? this.discoverMovies(): Observable.of([]);
   }
 
   fetchResults(recherche : string): Observable<Result[]>{
@@ -35,7 +40,43 @@ export class HomePage {
        params : {api_key : key, query : recherche}
      }).pluck("results");
     
-    }
+  }
+
+  private discoverMovies(): Observable<Result[]> {
+    return this.http.get<Result[]>("https://api.themoviedb.org/3/discover/movie", {
+       params : {api_key : key,primary_release_year : '2019'}
+     }).pluck("results");
+  }
+
+  private showRandomMovieAlert(movies : Result[]):void{
+    var movie = movies[Math.floor(Math.random()*movies.length)];
+    if(!movie){return;}
+    let alert = this.alert.create({
+      title: movie.title,
+      message: movie.overview,
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: 'Details',
+          handler: () => {
+            this.navCtrl.push(this.pushPage, {film : movie});
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  ionViewDidEnter() {
+    this.shakeSubscription = Observable.fromPromise(this.platform.ready()).switchMap(() => this.shake.startWatch()).switchMap(()=>this.discoverMovies()).subscribe(movies => this.showRandomMovieAlert(movies));
+  }
+
+  ionViewWillLeave(){
+    this.shakeSubscription.unsubscribe();
+  }
+
 }
 
 
